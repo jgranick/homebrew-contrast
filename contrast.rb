56ed24e8e4bf14dbc5b3bfa662ec96ddd34845cf
Jamie <jamie.mcgurk@contrastsecurity.com>
2022-04-07 16:37:52 +0100

    alpha commit

--- contrast.rb
+++ contrast.rb
@@ -1,17 +1,17 @@
 
-class contrast < Formula
+class Contrast < Formula
     desc "checks plumbing tests"
     homepage "https://www.contrastsecurity.com/"
     license "MIT"
   
     if OS.mac?
-      url ""
-      sha256 ""
+      url "https://github.com/Contrast-Security-OSS/contrast/releases/download/alpha-release/contrast-macos"
+      sha256 "1cb21076b55e9a6dc2688b72ccccd6cc2d39ac884bf3c73b5b915c1340b816f0"
       
     elsif OS.linux?
       if Hardware::CPU.intel?
-        url ""
-        sha256 ""
+        url "https://github.com/Contrast-Security-OSS/contrast/releases/download/alpha-release/contrast-linux"
+        sha256 "41234350aa93ba4b5a53492c928aaeee649bbdba4dc6802482722685ba95b355"
       end
     end
   
