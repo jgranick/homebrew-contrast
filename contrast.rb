b27918e1606dbc3d5c137657114767ece8268fa0
Jamie <jamie.mcgurk@contrastsecurity.com>
2022-04-27 10:28:11 +0100

    wip

--- contrast.rb
+++ contrast.rb
@@ -5,13 +5,13 @@ class Contrast < Formula
     license "MIT"
   
     if OS.mac?
-      url "https://github.com/Contrast-Security-OSS/homebrew-contrast/releases/download/v0.0.0/contrast-macos"
-      sha256 "1cb21076b55e9a6dc2688b72ccccd6cc2d39ac884bf3c73b5b915c1340b816f0"
+      url "https://github.com/Contrast-Security-OSS/homebrew-contrast/releases/download/v1.0.0/contrast-macos"
+      sha256 "a7c1b524d7f6056acaa26caa0a03294154b7daa0f0fd14e03bb5e2adb6833dd2"
       
     elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/Contrast-Security-OSS/contrast/releases/download/v0.0.0/contrast-linux"
-        sha256 "41234350aa93ba4b5a53492c928aaeee649bbdba4dc6802482722685ba95b355"
+        url "https://github.com/Contrast-Security-OSS/contrast/releases/download/v1.0.0/contrast-linux"
+        sha256 "901ad995daa47e97ee93efcca5735911c1b9409683908a3dace65bf3f647edac"
       end
     end
   
